45dfb8d093d65ee095591328974eb383be2369c3
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 20:14:38 +0100

    Add files via upload

---
+++ tri_à_bulles_entiers_relatifs.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+ 
+def triBulles(lst):
+  for i in range(len(lst)):
+    k = i
+    while (k > 0 and lst[k-1] > lst[k]):
+      lst[k-1], lst[k] = lst[k], lst[k-1]
+      k = k - 1
+      return lst
+      
+lst = [1, -7, 10, 4, 2, 3, -5, 9, 6, 8, 6, 0]
+print (“Liste d’entiers relatifs avant le tri :”, lst)
+print (“Liste d’entiers relatifs après le tri :”, triBulles(lst))
+
+
+-------------------------------------------------------------------------------- Résultat --------------------------------------------------------------------------------------
